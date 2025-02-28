924c59df9cc8fe44aa2a5991676a44da155f31dd
shiRongJie <874393648@qq.com>
2025-02-28 14:50:42 +0800

    2

--- contract/FoundMe.sol
+++ contract/FoundMe.sol
@@ -16,12 +16,30 @@ contract FundMe {
 
     AggregatorV3Interface internal dataFeed;
 
-    constructor() {
+    //目标值
+    uint256 constant TARGET = 1000 * 10 ** 18;
+
+    //合约拥有者
+    address public owner;
+
+    //合约部署时间
+    uint256 deploymentTimestamp;
+    //锁定期
+    uint256 lockTime;
+
+    constructor(uint256 _lockTime) {
         dataFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        owner = msg.sender;
+        deploymentTimestamp = block.timestamp;
+        lockTime = _lockTime;
     }
 
+    /**
+     * 向合约发送Eth
+     */
     function fund() external payable {
         require(convertEthToUsd(msg.value) >= MINIMUM_VALUE, "Send more ETH, At least 1 ETH");
+        require(block.timestamp < deploymentTimestamp + lockTime, "window is closed!");
         fundersToAmount[msg.sender] = msg.value;
     }
 
@@ -43,6 +61,56 @@ contract FundMe {
     function convertEthToUsd(uint256 ethAmount) internal view returns(uint256) {
         uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
         return ethAmount * ethPrice / (10 ** 8);
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    /**
+     * 提款，三种方式：transfer、send、call，官方推荐call功能更强大
+     */
+    function getFund() external windowClosed onlyOwner {
+        require(convertEthToUsd(address(this).balance) >= TARGET, "TARGET is not reached!");
+        //transfer: transfer ETH and revert if tx failed
+        //payable(owner).transfer(address(this).balance);
+
+        //send: transfer ETH and return false if failed
+        //bool success = payable(owner).send(address(this).balance);
+        //require(success, "Payment Failed!");
+
+        //call: transfer ETH with data return value of function and bool 
+        bool success;
+        (success, /*returnData*/) = payable(owner).call{value: address(this).balance}("");
+        require(success, "transfer tx failed");
+        fundersToAmount[msg.sender] = 0;
+    }
+
+
+    /**
+     * 退款
+     */
+    function refund() external windowClosed {
+        require(convertEthToUsd(address(this).balance) < TARGET, "TARGET is reached!");
+        require(fundersToAmount[msg.sender] != 0, "there is no fund for you");
+        bool success;
+        (success, /*returnData*/) = payable(msg.sender).call{value: fundersToAmount[msg.sender]}("");
+        require(success, "transfer tx failed");
+        fundersToAmount[msg.sender] = 0;
+    }
+
+    /*
+     * _;放在后面时，以refund函数为例，先执行windowClosed的校验，通过后才执行refund；
+     * 放在前面，先执行refund，后执行windowClosed的校验
+     */
+    modifier windowClosed() {
+        require(block.timestamp >= deploymentTimestamp + lockTime, "window is not closed!");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "this function can only be called by owner");
+        _;
     }
 
 }
